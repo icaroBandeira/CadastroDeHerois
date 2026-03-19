583c15cd824d650462d9b4965d1b1bebc82911b4
icaro <icarobarros723@gmail.com>
2026-03-19 17:14:41 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroDeHerois/CadastroDeHeroisApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeHerois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeHeroisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeHeroisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeHerois/CadastroDeHeroisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeHerois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeHeroisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
