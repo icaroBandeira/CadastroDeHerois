f32f1d4fb0c17212309c12ce49b9b9378674be64
icaro <icarobarros723@gmail.com>
2026-03-20 14:57:56 -0300

    Criado o controller

---
+++ src/main/java/dev/java10x/CadastroDeHerois/Controller.java
@@ -0,0 +1,16 @@
+package dev.java10x.CadastroDeHerois;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping ("/teste")
+    public String teste () {
+        return "AAAAAAAAAAAAAAAAAAAAALOOOOOOOOOOOOOOOOOOOOOUUUUUUUUUUUUUUUUUUUU";
+    }
+
+}
