f231481cbdfb65a33fe55cbbb7d8f04a5b3be1ac
icaro <icarobarros723@gmail.com>
2026-03-23 22:34:41 -0300

    Cria uma entidade no banco de dados apartir do HeroiModel

---
+++ src/main/java/dev/java10x/CadastroDeHerois/HeroiModel.java
@@ -0,0 +1,59 @@
+package dev.java10x.CadastroDeHerois;
+
+import jakarta.persistence.*;
+
+/// Entity transforma uma classe em uma entidade do banco de dados
+@Entity
+@Table(name = "tb_cadastro")
+public class HeroiModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String funcao;
+    private String habilidadePrimaria;
+    private int nivelDoHeroi;
+
+    public HeroiModel() {
+    }
+
+    public HeroiModel(String nome, String funcao, String habilidadePrimaria, int nivelDoHeroi) {
+        this.nome = nome;
+        this.funcao = funcao;
+        this.habilidadePrimaria = habilidadePrimaria;
+        this.nivelDoHeroi = nivelDoHeroi;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public String getHabilidadePrimaria() {
+        return habilidadePrimaria;
+    }
+
+    public void setHabilidadePrimaria(String habilidadePrimaria) {
+        this.habilidadePrimaria = habilidadePrimaria;
+    }
+
+    public int getNivelDoHeroi() {
+        return nivelDoHeroi;
+    }
+
+    public void setNivelDoHeroi(int nivelDoHeroi) {
+        this.nivelDoHeroi = nivelDoHeroi;
+    }
+}
